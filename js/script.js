f588212de010fbba955135d1d83662f580b449cb
KoryakinaSvetlana <switch_sv@mail.ru>
2017-12-04 02:57:58 +0500

    Приведение отступов в соответствие макету. Подключена интерактивная карта

---
+++ js/script.js
@@ -0,0 +1,13 @@
+﻿window.onload = function () {
+    var menuService = document.querySelectorAll(".service-navigation .btn");
+    var writeUsBtn = document.querySelector(".contact .btn");
+
+    menuService.addEventListener("click", function (event) {
+        event.preventDefault();
+    });
+
+    writeUsBtn.addEventListener("click", function (event) {
+        event.preventDefault();
+    });
+
+};
