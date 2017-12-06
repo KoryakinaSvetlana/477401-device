8bffbb20122b5478f38960e9dad294642b177cd7
KoryakinaSvetlana <switch_sv@mail.ru>
2017-12-07 00:40:50 +0500

    Доработки по замечаниям и соотвествие дополнительным критериям. Оживление сайта с помощью js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,111 @@
 ﻿window.onload = function () {
-    var menuService = document.querySelectorAll(".service-navigation .btn");
-    var writeUsBtn = document.querySelector(".contact .btn");
+    var openPopupMap = document.querySelector("#openPopupMap");
+    var openWriteusForm = document.querySelector("#openWriteusForm");
 
-    menuService.addEventListener("click", function (event) {
-        event.preventDefault();
+    var modalMap = document.querySelector(".modal.modal-map");
+    var writeusForm = document.querySelector(".modal.modal-feedback");
+
+    var closeModalMap = document.querySelector(".modal-map .modal-close");
+    var closeWriteusForm = document.querySelector(".modal-feedback .modal-close");
+
+    var slideBtn1 = document.querySelector("#btn-1");
+    var slideBtn2 = document.querySelector("#btn-2");
+    var slideBtn3 = document.querySelector("#btn-3");
+
+    var deliveryBtn = document.querySelector("#deliveryBtn");
+    var warrantyBtn = document.querySelector("#warrantyBtn");
+    var creditBtn = document.querySelector("#creditBtn");
+
+    function removeActiveClass() {
+        var items = document.querySelectorAll(".service-navigation li");
+        items.forEach(function (item, i, arr) {
+            item.classList.remove("active");
+        })
+    }
+
+    function hideSliders() {
+        var items = document.querySelectorAll(".slider-item");
+        items.forEach(function (item, i, arr) {
+            item.classList.remove("active");
+        })
+    }
+
+    function hideServiceContents() {
+        var items = document.querySelectorAll(".service-item");
+        items.forEach(function (item, i, arr) {
+            item.classList.remove("active");
+        })
+    }
+
+    window.addEventListener("keydown", function (event) {
+        if (event.keyCode === 27) {
+            modalMap.classList.remove("modal-show");
+            writeusForm.classList.remove("modal-show");
+        }
     });
 
-    writeUsBtn.addEventListener("click", function (event) {
-        event.preventDefault();
+    slideBtn1.addEventListener("click", function (event) {
+        var item = document.querySelector("#firstSlide");
+        hideSliders();
+        item.classList.add("active");
     });
 
+    slideBtn2.addEventListener("click", function (event) {
+        var item = document.querySelector("#secondSlide");
+        hideSliders();
+        item.classList.add("active");
+    });
+
+    slideBtn3.addEventListener("click", function (event) {
+        var item = document.querySelector("#thirdSlide");
+        hideSliders();
+        item.classList.add("active");
+    });
+
+    deliveryBtn.addEventListener("click", function (event) {
+        event.preventDefault();
+        var item = document.querySelector("#deliveryContent");
+        hideServiceContents();
+        removeActiveClass();
+        item.classList.add("active");
+        this.parentElement.classList.add("active");
+    });
+
+    warrantyBtn.addEventListener("click", function (event) {
+        event.preventDefault();
+        var item = document.querySelector("#warrantyContent");
+        hideServiceContents();
+        removeActiveClass();
+        item.classList.add("active");
+        this.parentElement.classList.add("active");
+    });
+
+    creditBtn.addEventListener("click", function (event) {
+        event.preventDefault();
+        var item = document.querySelector("#creditContent");
+        hideServiceContents();
+        removeActiveClass();
+        item.classList.add("active");
+        this.parentElement.classList.add("active");
+    });
+
+    closeWriteusForm.addEventListener("click", function (event) {
+        event.preventDefault();
+        writeusForm.classList.remove("modal-show");
+    });
+
+    closeModalMap.addEventListener("click", function (event) {
+        event.preventDefault();
+        modalMap.classList.remove("modal-show");
+    });
+
+    openWriteusForm.addEventListener("click", function (event) {
+        event.preventDefault();
+        writeusForm.classList.add("modal-show");
+    });
+
+    openPopupMap.addEventListener("click", function (event) {
+        event.preventDefault();
+        modalMap.classList.add("modal-show");
+    });
 };
